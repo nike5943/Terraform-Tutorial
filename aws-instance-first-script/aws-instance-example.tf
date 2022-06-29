8e01f14a40605383568eead8a396fcd541fb5ea8
nike5943 <65210837+nike5943@users.noreply.github.com>
2022-06-29 22:35:07 +0530

    Update aws-instance-example.tf

--- aws-instance-first-script/aws-instance-example.tf
+++ aws-instance-first-script/aws-instance-example.tf
@@ -1,4 +1,13 @@
-resource "aws_instance" "web1" {
-   ami           = "${lookup(var.ami_id, var.region)}"
+provider "aws"{
+     region = "ap-south-1"
+}
+resource "aws_instance""server1"{
+   count = "1"
+   ami = "ami-074251216af698218"
    instance_type = "t2.micro"
- }
+   key_name = "devops_ec2_jenkins"
+   tags={
+    Name = "serverec2"
+    Env = "dev"
+}
+}
